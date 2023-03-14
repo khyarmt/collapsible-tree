af70ba4108606c1051cf602811b2c3b2c7b953c6
khyarmt <khyarmt@gmail.com>
2023-03-14 18:30:16 +0900

    Add data import script

--- src/App.js
+++ src/App.js
@@ -1,4 +1,18 @@
+import { useEffect } from "react";
+import * as d3 from "d3";
+
 function App() {
+  useEffect(() => {
+    (async () => {
+      const response = await fetch("data/flare.csv");
+      const data = d3.csvParse(await response.text());
+      for (const item of data) {
+        const segments = item.id.split(".");
+        item.name = segments[segments.length - 1];
+      }
+    })();
+  }, []);
+
   return (
     <>
       <div>Collapsible Tree</div>
